4499246719f16a121ed8cb07e879d55ef60db79c
Muhammad Bin Usman <muhammadbin.2003@gmail.com>
2024-03-09 23:24:47 +0500

    Create runpod.sh

---
+++ runpod.sh
@@ -0,0 +1,2 @@
+curl -O -L https://gist.githubusercontent.com/MuhammadBinUsman03/eeb2a3775c5e16b55caabbefd927f862/raw/Gguf.py
+python Gguf.py --MODEL_ID $MODEL_ID --QUANTIZATION_METHODS $QUANTIZATION_METHODS --USERNAME $USERNAME --HF_TOKEN $TOKEN
